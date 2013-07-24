4035b64dfc19dfb298d0d34e339519d9ad181f1e
Flurin Rindisbacher <info@flurischt.ch>
2013-07-24 23:04:04 +0200

    set the path to the ffmpeg executable

--- run_tests.py
+++ run_tests.py
@@ -4,7 +4,11 @@
 import os
 import unittest
 
+#make sure, python can find your ffmpeg installation (for pydub)
+PATH_TO_FFMPEG = '/usr/local/bin/'
+
 if __name__ == '__main__':
+    os.environ['PATH'] = os.environ['PATH'] + ':' + PATH_TO_FFMPEG
     dir = os.path.dirname(os.path.realpath(__file__))
     suite = unittest.TestLoader().discover(dir, pattern = "test_*.py")
     unittest.TextTestRunner(verbosity=2).run(suite) 
