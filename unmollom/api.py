39bc110255a678fe5d19e8665948df42e3679acb
Flurin Rindisbacher <info@flurischt.ch>
2013-08-01 16:11:17 +0200

    add the API functions and an example script

--- unmollom/api.py
+++ unmollom/api.py
@@ -5,8 +5,17 @@ unmollom.api
 
 
 """
-def solve_url(session, url):
-    pass
 
-def solve(session, htmlsource):
-    pass
+from unmollom.util import *
+from unmollom.speech_recognition import *
+
+def solve_url(requests_session, url):
+    html = requests_session.get(url).content
+    return solve(requests_session, html)
+
+def solve(requests_session, htmlsource):
+    url = extract_mollom_audio_file(htmlsource)
+    mp3 = requests_session.get(url).content
+    speech = recognize(mp3)
+    captcha =  build_captcha(speech['text'])
+    return captcha
